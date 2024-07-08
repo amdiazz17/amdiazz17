8c05bf1e255f67e3863b617ce10bede6412583dc
Ava Diaz <amdiazz17@Avas-MacBook-Pro.local>
2024-07-08 11:30:14 -0500

    new stuff

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+
+function App() {
+const ava= {
+    height: "5'5",
+    hair: "Straight",
+}
+
+    return(
+        <div classsName="App">
+            {ava.hair}
+        </div>
+    )
+
+}
+
+
+
+export default App;
